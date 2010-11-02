cd7b7b7c1365b6f8c9ad05e3633debda535636a7
James Harton <jamesotron@gmail.com>
2010-11-02 13:47:50 +1300

    Custom ruby script to add the machine to DNS via the linode DNS api.

---
+++ linode_dns_update/metadata.rb
@@ -0,0 +1,7 @@
+maintainer "James Harton, Sociable Limited."
+maintainer_email "james@sociable.co.nz"
+license "MIT"
+description "Updates DNS on linode.com with the machines IP address and hostname."
+version "0.0.1"
+
+recipe "linode_dns_update", "Update linode DNS."
---
+++ linode_dns_update/recipes/default.rb
@@ -0,0 +1,30 @@
+gem_package 'linode' do
+  not_if "test -e /usr/local/bin/rvm-gem.sh"
+end
+
+gem_package "linode" do
+  gem_binary "/usr/local/bin/rvm-gem.sh"
+  only_if "test -e /usr/local/bin/rvm-gem.sh"
+end
+
+
+template '/root/.linode.yml' do
+  owner 'root'
+  group 'root'
+  mode 0600
+  source 'linode.yml.erb'
+  only_if { node[:linode][:api_key] }
+end
+
+cookbook_file '/usr/local/bin/update_linode_dns.rb' do
+  owner 'root'
+  group 'root'
+  mode 0755
+end
+
+bash 'update_linode_dns' do
+  user 'root'
+  only_if "test -e /usr/local/bin/update_linode_dns.rb"
+  only_if "test -e /root/.linode.yml"
+  code "/usr/local/bin/update_linode_dns.rb"
+end
