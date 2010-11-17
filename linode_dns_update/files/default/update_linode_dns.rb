679799a00b0240d637610cf180b077b5571527ea
James Harton <jamesotron@gmail.com>
2010-11-18 11:11:58 +1300

    Change the DNS update script to take a hostname and address as a commnad line argument instead of intuiting them itself. This allows the creation of internal zones for private interfaces.

--- linode_dns_update/files/default/update_linode_dns.rb
+++ linode_dns_update/files/default/update_linode_dns.rb
@@ -6,8 +6,8 @@ require 'linode'
 
 @linode = Linode.new(:api_key => (YAML::load_file File.expand_path('~/.linode.yml'))['linode']['api_key'])
 
-my_host_name = Socket.gethostbyname(Socket.gethostname).first
-#my_host_name = 'hostname.tst.kimonoapp.com'
+my_host_name = ARGV[0]
+my_address = ARGV[1]
 
 def get_domain(hostname)
   chunked = hostname.split('.').reverse
@@ -23,39 +23,29 @@ def get_domain(hostname)
   nil
 end
 
+def get_node(fqdn, domain=nil)
+  ((fqdn.split('.') - (domain||get_domain(fqdn)).split('.'))) * '.'
+end
+
 def get_rr(hostname, type, domain)
-  node = ((hostname.split('.') - domain.domain.split('.'))) * '.'
   @linode.domain.resource.list( 'DOMAINID' => domain.domainid ).each do |rr|
-    return rr if rr.name == node && rr.type == type
+    return rr if rr.name == get_node(hostname, domain.domain) && rr.type == type
   end
   nil
 end
 
-def get_local_ip
-  orig, Socket.do_not_reverse_lookup = Socket.do_not_reverse_lookup, true  # turn off reverse DNS resolution temporarily
-
-  UDPSocket.open do |s|
-    s.connect '64.233.187.99', 1
-    s.addr.last
-  end
-ensure
-  Socket.do_not_reverse_lookup = orig
-end
-
 if linode_domain = get_domain(my_host_name)
-  local_ip = get_local_ip
-  node = ((my_host_name.split('.') - linode_domain.domain.split('.'))) * '.'
   if rr = get_rr(my_host_name, 'A', linode_domain)
     puts "Host #{my_host_name} found in zone #{linode_domain.domain}."
-    if rr.target != local_ip
-      @linode.domain.resource.update  'DOMAINID' => linode_domain.domainid, 'RESOURCEID' => rr.resourceid, 'TARGET' => local_ip
-      puts "[updated] #{my_host_name} IN A #{local_ip}"
+    if rr.target != my_address
+      @linode.domain.resource.update  'DOMAINID' => linode_domain.domainid, 'RESOURCEID' => rr.resourceid, 'TARGET' => my_address
+      puts "[updated] #{my_host_name} IN A #{my_address}"
     else
       puts "[noop]"
     end
   else
-    @linode.domain.resource.create  'DOMAINID' => linode_domain.domainid, 'TYPE' => 'A', 'TARGET' => local_ip, 'NAME' => node
-    puts "[create] #{my_host_name} IN A #{local_ip}"
+    @linode.domain.resource.create  'DOMAINID' => linode_domain.domainid, 'TYPE' => 'A', 'TARGET' => my_address, 'NAME' => get_node(my_host_name, linode_domain.domain)
+    puts "[create] #{my_host_name} IN A #{my_address}"
   end
 else
   puts "Error, no matching domain."
--- linode_dns_update/recipes/default.rb
+++ linode_dns_update/recipes/default.rb
@@ -22,9 +22,25 @@ cookbook_file '/usr/local/bin/update_linode_dns.rb' do
   mode 0755
 end
 
-bash 'update_linode_dns' do
+# Flagrant debianism.
+package 'bind9-host'
+
+bash 'update_external_dns' do
   user 'root'
   only_if "test -e /usr/local/bin/update_linode_dns.rb"
   only_if "test -e /root/.linode.yml"
-  code "/usr/local/bin/update_linode_dns.rb"
+  only_if "test $( host -t A #{node[:fqdn]} | awk '{print \"x\" $4}' ) == 'x#{node[:ipaddress]}'"
+  code "/usr/local/bin/update_linode_dns.rb #{node[:fqdn]} #{node[:ipaddress]}"
+end
+
+# FIXME: patch to handle EC2 internal addresses too.
+bash 'update_internal_dns' do
+  user 'root'
+  only_if "test -e /usr/local/bin/update_linode_dns.rb"
+  only_if "test -e /root/.linode.yml"
+  only_if { !!node[:rackspace] }
+  internal_hostname = "#{node[:hostname]}.int.rs.#{node[:domain]}"
+  internal_address = node[:rackspace][:private_ip]
+  only_if "test $( host -t A #{internal_hostname} | awk '{print \"x\" $4}' ) == 'x#{internal_address}'"
+  code "/usr/local/bin/update_linode_dns.rb #{internal_hostname} #{internal_address}"
 end
