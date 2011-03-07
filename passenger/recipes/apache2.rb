9a727be71352a376ffe86b30b417dbe99bf6c96f
seph <seph@directionless.org>
2011-03-08 02:47:03 +0800

    Added apache2 support to passenger

---
+++ passenger/recipes/apache2.rb
@@ -0,0 +1,75 @@
+#
+# Cookbook Name:: passenger
+# Recipe:: apache2
+
+# Derives from the opsware passenger_apache2
+
+include_recipe "passenger::install"
+include_recipe "apache2"
+
+
+if platform?("centos","redhat")
+  if dist_only?
+    # just the gem, we'll install the apache module within apache2
+    package "rubygem-passenger"
+    return
+  else
+    package "httpd-devel"
+  end
+else
+  %w{ apache2-prefork-dev libapr1-dev }.each do |pkg|
+    package pkg do
+      action :upgrade
+    end
+  end
+end
+
+
+rvm_exec_prefix = system("test -e /usr/local/rvm") ? "/usr/local/bin/rvm default exec" : ""
+
+
+bash "install passenger/apache2" do
+  user "root"
+  code '#{rvm_exec_prefix} passenger-install-apache2-module --auto'
+  not_if "test -e #{node[:passenger][:module_path]}"
+end
+
+
+
+if platform?("centos","redhat") and dist_only?
+  package "mod_passenger" do
+    notifies :run, resources(:execute => "generate-module-list"), :immediately
+  end
+
+  file "#{node[:apache][:dir]}/conf.d/mod_passenger.conf" do
+    action :delete
+    backup false
+  end
+else
+  template "#{node[:apache][:dir]}/mods-available/passenger.load" do
+    cookbook "passenger"
+    source "apache.load.erb"
+    owner "root"
+    group "root"
+    mode 0644
+  end
+end
+
+template "#{node[:apache][:dir]}/mods-available/passenger.conf" do
+  cookbook "passenger"
+  source "apache.conf.erb"
+  owner "root"
+  group "root"
+  mode 0644
+end
+
+apache_module "passenger"
+
+
+#log_path = node[:passenger][:production][:log_path]
+#
+#directory log_path do
+#  mode 0755
+#  action :create
+#end
+
--- passenger/recipes/install.rb
+++ passenger/recipes/install.rb
@@ -4,11 +4,13 @@
 
 gem_package "passenger/system" do
   package_name 'passenger'
+  version node[:passenger][:version]
   not_if "test -e /usr/local/bin/rvm-gem.sh"
 end
 
 gem_package "passenger/rvm" do
   package_name 'passenger'
+  version node[:passenger][:version]
   gem_binary "/usr/local/bin/rvm-gem.sh"
   only_if "test -e /usr/local/bin/rvm-gem.sh"
 end
@@ -23,3 +25,6 @@ gem_package "bundler/rvm" do
   gem_binary "/usr/local/bin/rvm-gem.sh"
   only_if "test -e /usr/local/bin/rvm-gem.sh"
 end
+rvm_exec_prefix = system("test -e /usr/local/rvm") ? "/usr/local/bin/rvm default exec" : ""
+node.default[:passenger][:root_path] = `#{rvm_exec_prefix} passenger-config --root`.chomp 
+node.default[:passenger][:module_path] = `#{rvm_exec_prefix} passenger-config --root`.chomp + "/ext/apache2/mod_passenger.so"
