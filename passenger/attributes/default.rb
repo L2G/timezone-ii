bc88239ecd62cad60193ef43bb706c8b0a1ed4fb
seph <seph@directionless.org>
2011-03-18 05:50:37 +0800

    More passenger options

---
+++ passenger/attributes/default.rb
@@ -0,0 +1,6 @@
+default[:passenger][:conf][:max_pool_size] = 10
+default[:passenger][:conf][:max_requests] = 1000
+#default[:passenger][:conf][:min_instances] = 
+#default[:passenger][:conf][:max_instances_per_app] =
+#default[:passenger][:conf][:pool_idle_time]
+default[:passenger][:conf][:user_switching] = "off"
