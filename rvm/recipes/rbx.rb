79fc8351e8a4ceaa673bad83ffe1cc9752fdda30
James Harton <jamesotron@gmail.com>
2011-11-10 10:10:18 +1300

    Add other implementation helper recipes and add the deps that RVM asks for.

--- rvm/recipes/rbx.rb
+++ rvm/recipes/rbx.rb
@@ -1,12 +1,11 @@
 #
 # Cookbook Name:: rvm
-# Recipe:: ruby_187
+# Recipe:: rbx
 
 # Install deps as listed by recent revisions of RVM.
 %w(build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion).each do |pkg|
   package pkg
 end
 
-node.default[:rvm][:ruby][:implementation] = 'ruby'
-node.default[:rvm][:ruby][:version] = '1.8.7'
+node.default[:rvm][:ruby][:implementation] = 'rbx'
 include_recipe "rvm::install"
--- rvm/recipes/ruby_193.rb
+++ rvm/recipes/ruby_193.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: rvm
-# Recipe:: ruby_187
+# Recipe:: ruby_193
 
 # Install deps as listed by recent revisions of RVM.
 %w(build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion).each do |pkg|
@@ -8,5 +8,5 @@
 end
 
 node.default[:rvm][:ruby][:implementation] = 'ruby'
-node.default[:rvm][:ruby][:version] = '1.8.7'
+node.default[:rvm][:ruby][:version] = '1.9.3'
 include_recipe "rvm::install"
