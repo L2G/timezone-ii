30988b0765742084bf4c973e80e9026f1ef7da87
James Harton <jamesotron@gmail.com>
2011-11-11 11:43:23 +1300

    Be more specific about deps because of chef package resource != apt-get.

--- rvm/recipes/rbx.rb
+++ rvm/recipes/rbx.rb
@@ -3,7 +3,7 @@
 # Recipe:: rbx
 
 # Install deps as listed by recent revisions of RVM.
-%w(build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion).each do |pkg|
+%w(build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev autoconf libc6-dev libncurses5-dev automake libtool bison subversion).each do |pkg|
   package pkg
 end
 
