aa5c1ada6ce2bae5276a4256dceba06b92bc36ed
garren smith <garren.smith@gmail.com>
2012-06-03 13:20:37 +0300

    Update to new rvm install url

--- rvm/recipes/default.rb
+++ rvm/recipes/default.rb
@@ -24,7 +24,7 @@ end
  
 bash "installing system-wide RVM stable" do
   user "root"
-  code "bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)"
+  code "curl -L get.rvm.io | sudo bash -s stable; echo"
   not_if "test -e /usr/local/rvm/bin/rvm"
 end
 
