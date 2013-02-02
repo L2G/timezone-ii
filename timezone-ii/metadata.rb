af0cc197ad5ed6d364108be6841a0b38dac5e835
Larry Gilbert <larry@l2g.to>
2013-02-02 13:36:50 -0800

    timezone-ii version 0.1.3

--- timezone-ii/metadata.rb
+++ timezone-ii/metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Lawrence Leonard Gilbert"
 maintainer_email "larry@L2G.to"
 license          "Apache 2.0"
 description      "Configure the system timezone on Linux systems"
-version          "0.1.2"
+version          "0.1.3"
 
 # Should support any OS in the "debian" and "rhel" families, as defined by Ohai
 %w{ amazon centos debian gentoo enterpriseenterprise linuxmint oracle raspbian
