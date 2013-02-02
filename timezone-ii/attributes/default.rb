31ec92fe12868850a1dcca4e17baac25b3a0b9e7
Larry Gilbert <larry@l2g.to>
2013-02-02 13:18:20 -0800

    New planned default attributes

--- timezone-ii/attributes/default.rb
+++ timezone-ii/attributes/default.rb
@@ -1,1 +1,10 @@
 default[:tz] = 'UTC'
+
+# Path to tzdata directory
+default['timezone']['tzdata_dir'] => '/usr/share/zoneinfo'
+
+# Path to file used by kernerl for local timezone's data
+default['timezone']['localtime_path'] => '/etc/localtime'
+
+# Whether to use a symlink to tzdata (instead of copying)
+default['timezone']['use_symlink'] => false
