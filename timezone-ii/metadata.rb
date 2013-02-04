cb7d737abcaef86e034368009f5ff32bef1ede76
Larry Gilbert <larry@l2g.to>
2013-02-03 18:03:07 -0800

    timezone-ii version 0.1.5 (with extra metadata)
    New metadata:
    * This cookbook is intended to replace 'timezone' version 0.0.1 (since
      it's a fork of that cookbook)
    * Specific versions of Fedora, Amazon Linux, and Gentoo indicated

--- timezone-ii/metadata.rb
+++ timezone-ii/metadata.rb
@@ -2,10 +2,18 @@ maintainer       "Lawrence Leonard Gilbert"
 maintainer_email "larry@L2G.to"
 license          "Apache 2.0"
 description      "Configure the system timezone on Linux systems"
-version          "0.1.4"
+version          "0.1.5"
 
-# Should support any OS in the "debian" and "rhel" families, as defined by Ohai
-%w{ amazon centos debian gentoo enterpriseenterprise linuxmint oracle raspbian
-    redhat scientific ubuntu }.each do |os|
+replaces         "timezone", "= 0.0.1"
+
+# These are platform versions where this cookbook has been tested at some point
+# in time
+supports "amazon", ">= 2012.09"
+supports "fedora", ">= 18"
+supports "gentoo", ">= 2.1"
+
+# These have not been tested but should also work
+%w{ centos debian enterpriseenterprise linuxmint oracle raspbian redhat
+    scientific ubuntu }.each do |os|
   supports os
 end
