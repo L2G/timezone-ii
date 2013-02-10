7f4ba7c479613abe30bc1f92a22d51513f2b54ca
Larry Gilbert <larry@l2g.to>
2013-02-10 00:27:40 -0800

    Log messages in a way friendly to Chef 10.x

--- timezone-ii/recipes/default.rb
+++ timezone-ii/recipes/default.rb
@@ -8,11 +8,6 @@
 # Apache 2.0 License.
 #
 
-log 'timezone setting' do
-  message "Time zone setting: #{node.tz}"
-  level :debug
-end
-
 # Make sure the tzdata database is installed. (Arthur David Olson, the computer
 # timekeeping field is forever in your debt.)
 package value_for_platform_family(
@@ -29,9 +24,9 @@ else
     # Load the generic Linux recipe if there's no better known way to change the
     # timezone.  Log a warning (unless this is known to be the best way on a
     # particular platform).
-    log "fallback Linux" do
-      message "Linux platform '#{node.platform}' is unknown to this recipe; " +
+    message = "Linux platform '#{node.platform}' is unknown to this recipe; " +
               "using fallback Linux method"
+    log message do
       level :warn
       not_if { %w( gentoo rhel ).include? node.platform_family }
     end
@@ -39,9 +34,9 @@ else
     include_recipe 'timezone-ii::linux-generic'
 
   else
-    log "unknown platform" do
-      message "Don't know how to configure timezone for " +
+    message = "Don't know how to configure timezone for " +
               "'#{node.platform_family}'!"
+    log message do
       level :error
     end
 
