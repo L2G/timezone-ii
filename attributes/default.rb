2f2218b1882646ab438a7a2f089c454da2e5c3ef
Larry Gilbert <larry@l2g.to>
2013-02-15 21:33:11 -0800

    Fix spelling and add a little more info

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,8 +3,9 @@ default[:tz] = 'UTC'
 # Path to tzdata directory
 default['timezone']['tzdata_dir'] = '/usr/share/zoneinfo'
 
-# Path to file used by kernerl for local timezone's data
+# Path to file used by kernel for local timezone's data
 default['timezone']['localtime_path'] = '/etc/localtime'
 
-# Whether to use a symlink to tzdata (instead of copying)
+# Whether to use a symlink to tzdata (instead of copying).
+# Used only in the linux-default recipe.
 default['timezone']['use_symlink'] = false
