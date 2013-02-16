7ee7d52c5324297e0d8c4bc7537dbaec87618d8b
Larry Gilbert <larry@l2g.to>
2013-02-16 00:17:02 -0800

    Version 0.1.6, and a couple of specific OS versions

--- metadata.rb
+++ metadata.rb
@@ -3,18 +3,20 @@ maintainer       "Lawrence Leonard Gilbert"
 maintainer_email "larry@L2G.to"
 license          "Apache 2.0"
 description      "Configure the system timezone on Linux systems"
-version          "0.1.5"
+version          "0.1.6"
 
 replaces         "timezone"
 
 # These are platform versions where this cookbook has been tested at some point
 # in time
 supports "amazon", ">= 2012.09"
+supports "centos", ">= 5.8"
 supports "fedora", ">= 18.0"
 supports "gentoo", ">= 2.1"
+supports "ubuntu", ">= 10.04"
 
 # These have not been tested but should also work
-%w{ centos debian enterpriseenterprise linuxmint oracle raspbian redhat
-    scientific ubuntu }.each do |os|
+%w{ debian enterpriseenterprise linuxmint oracle raspbian redhat
+    scientific }.each do |os|
   supports os
 end
