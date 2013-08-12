e1444ed224835ec9d6e6f0dad32d482242eef16a
frad00r4 <frad00r4@gmail.com>
2013-08-12 21:38:35 +0400

    Rhel files

---
+++ recipes/rhel.rb
@@ -0,0 +1,22 @@
+#
+# Cookbook Name:: timezone-ii
+# Recipe:: CentOS 6
+#
+# Copyright 2013, Lawrence Leonard Gilbert <larry@L2G.to>
+#
+# Apache 2.0 License.
+#
+
+template "/etc/sysconfig/clock" do
+  source "clock.erb"
+  owner 'root'
+  group 'root'
+  mode 0644
+  notifies :run, 'bash[tzdata-update]'
+end
+
+bash 'tzdata-update' do
+  user 'root'
+  code "/usr/sbin/tzdata-update"
+  action :nothing
+end
