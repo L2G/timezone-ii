1eaa1b5d6ff1f177d0e8518be7b5a03879827cef
Elan Ruusamäe <glen@delfi.ee>
2013-11-27 01:01:57 +0200

    notify immediately, it's good to have timezone affect immediately

--- recipes/pld.rb
+++ recipes/pld.rb
@@ -17,7 +17,7 @@ template '/etc/sysconfig/timezone' do
   owner 'root'
   group 'root'
   mode 0644
-  notifies :restart, 'service[timezone]'
+  notifies :restart, 'service[timezone]', :immediately
 end
 
 service 'timezone' do
