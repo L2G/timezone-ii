5b1b62e2217f5aa23120528feb1d0dde34f349b8
Scott Marshall <scott.marshall@johnmuirhealth.com>
2014-07-25 15:29:52 -0700

    Sent Centos and RHEL to the RHEL recipe

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Lawrence Leonard Gilbert"
 maintainer_email "larry@L2G.to"
 license          "Apache 2.0"
 description      "Configure the system timezone on Linux systems"
-version          "0.2.0"
+version          "0.2.1"
 
 replaces         "timezone"
 
