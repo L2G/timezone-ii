7ea19a4f11f5c176a6b2ba63287e74e2a20a9a1e
Corey Hemminger <corey.hemminger@nativex.com>
2017-04-11 15:08:28 -0500

    fix for chef-client 13

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Lawrence Leonard Gilbert'
 maintainer_email 'larry@L2G.to'
 license 'Apache 2.0'
 description' Configure the system timezone on *ix systems'
-version '0.2.9992'
+version '0.2.0'
 
 replaces 'timezone'
 
@@ -16,4 +16,4 @@ end
 source_url 'https://github.com/L2G/timezone-ii' if defined?(:source_url)
 issues_url 'https://github.com/L2G/timezone-ii/issues' if defined?(:issues_url)
 
-chef_version '>= 12'
+chef_version '>= 12.1' if respond_to?(:chef_version)
